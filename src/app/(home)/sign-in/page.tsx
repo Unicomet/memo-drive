5386a8ddb0d16927e93d970ac5860d753befc740
Guillermo Montano <martinezguillermo559@gmail.com>
2025-09-18 21:51:09 -0600

    Put sign in as button

--- src/app/(home)/sign-in/page.tsx
+++ src/app/(home)/sign-in/page.tsx
@@ -1,10 +1,13 @@
-import { SignedOut, SignInButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+import { Button } from "~/components/ui/button";
 
 export default function SingInPage() {
   return (
     <>
       <div className="mb-8 text-3xl">Sign In Page</div>
-      <SignInButton forceRedirectUrl={"/drive"} />
+      <Button asChild>
+        <SignInButton forceRedirectUrl={"/drive"} />
+      </Button>
       <footer className="mt-12">
         © {new Date().getFullYear()} Memo Drive. All rights reserved.
       </footer>
